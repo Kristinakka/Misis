7e539a6b045f584be87e21f0e3f852e80fd325bf
Kristinakka <fastopt07@gmail.com>
2025-09-30 12:59:29 +0300

    Add files via upload

---
+++ src/lab01/l3.py
@@ -0,0 +1,9 @@
+price = int(input("price:"))
+discount = int(input("discount:"))
+vat = int(input("vat:"))
+base = price * (1 - discount / 100)
+vat_amount = base * (vat / 100)
+total = base + vat_amount
+print("База после скидки:", base)
+print("НДС:", vat_amount)
+print("Итого к оплате:", total)
