1c86adb12fd6ca5df1d078aaa0d98102861e141d
Kristinakka <fastopt07@gmail.com>
2025-09-30 13:44:33 +0300

    Update l2.py
    Исправлено 2 задание

--- src/lab01/l2.py
+++ src/lab01/l2.py
@@ -1,5 +1,6 @@
-ch1 = float(input("a:"))
-ch2 = float(input("b:"))
+ch1 = input("a:")
+ch2 = input("b:")
+ch1, ch2 = float(ch1.replace(",", ".")), float(ch2.replace(",", "."))
 sum = ch1 + ch2
 sr = sum / 2
 print(sum)
