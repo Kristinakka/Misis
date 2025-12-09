0536841642e76a0969af39d00f5a6f26f3c1e7d3
Kristinakka <fastopt07@gmail.com>
2025-12-09 11:38:33 +0300

    Create analize_text.py

---
+++ src/lib/analize_text.py
@@ -0,0 +1,20 @@
+import sys
+
+sys.path.append(r"C:\Users\First\Documents\GitHub\python_labs\src\lib")
+# добавляем путь к папке lib, чтобы Python мог найти наши модули
+
+from GitHub.python_labs.src.lib.text import *
+
+# импортируем ВСЕ функции из файла text_3.py
+
+
+def analyze_text(text: str) -> None:
+    """создаем функцию, которая принимает текст и ничего не возвращает"""
+    print(f"Всего слов: {len(tokenize(normalize(text)))}")
+    print(f"Уникальных слов: {len(count_freq(tokenize(normalize(text))))}")
+    print(f"Топ-5:")
+    c = 0
+    for i in top_n(tokenize(normalize(text))):
+        c += 1
+        if c == 5:
+            break
